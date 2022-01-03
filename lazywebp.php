24f3701fff424543bf0f276c3fca102a664dccb9
Erik Yo <erikyo83@gmail.com>
2022-01-03 20:30:55 +0100

    defines constants to configure webp compression

--- lazywebp.php
+++ lazywebp.php
@@ -21,6 +21,11 @@ if ( !defined( 'EASYLAZY_ENABLED_EXTENSIONS' ) ) {
 // Display a fancy animation when showing the image (may score worst in CLP)
 if ( !defined( 'EASYLAZY_ANIMATED' ) ) define( 'EASYLAZY_ANIMATED', true );
 
+// Halve the quality of the images automatically if the width or the height is bigger than this value (because they are likely to be hdpi)
+if ( !defined( 'EASYLAZY_LARGE_IMAGE_LIMIT' ) ) define( 'EASYLAZY_LARGE_IMAGE_LIMIT', 1920 );
+if ( !defined( 'EASYLAZY_LARGE_IMAGE_QUALITY_PERCENTUAL' ) ) define( 'EASYLAZY_LARGE_IMAGE_QUALITY_PERCENTUAL', .5 ); // .5 means 50%
+if ( !defined( 'EASYLAZY_DEFAULT_JPG_COMPRESSION' ) ) define( 'EASYLAZY_DEFAULT_JPG_COMPRESSION', 82 );
+
 
 // Define support for webp images on plugin load
 if( !empty($_SERVER['HTTP_ACCEPT']) && strpos( $_SERVER['HTTP_ACCEPT'], 'image/webp' ) !== false ) {
