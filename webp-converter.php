27051c73aee0ac36255590108d2c3720ea15f98f
Erik Yo <erikyo83@gmail.com>
2022-01-03 23:23:03 +0100

    updated version that stores webp images into wordpress media array (ref. https://github.com/WordPress/performance/pull/32#issuecomment-1003632432)

--- webp-converter.php
+++ webp-converter.php
@@ -11,110 +11,115 @@ function easylazy_explode_filepath( $filepath ) {
 	}
 }
 
-function easylazy_explode_filename( $filename ) {
-	$filename_parts = explode( '.', $filename );
-	$fext           = $filename_parts[ count( $filename_parts ) - 1 ];
-	unset( $filename_parts[ count( $filename_parts ) - 1 ] );
-	$fname = implode( '.', $filename_parts );
-
-	return array( $fname, $fext );
+function easylazy_is_large_image( $width, $height ) {
+	$large_image_size = apply_filters('easylazy_large_image_size', EASYLAZY_LARGE_IMAGE_LIMIT);
+	return max(array($width, $height)) > $large_image_size;
 }
 
 function easylazy_save_webp_copy( $metadata, $attachment_id ) {
 
-	list( $basedir, $path, $filename ) = easylazy_explode_filepath( $metadata['file'] );
-	list( $fname, $fext ) = easylazy_explode_filename( $filename );
+	// PHP GD is required
+	if ( ! extension_loaded( 'gd' ) ) return $metadata;
 
-	if ( isset( $metadata['mime-type'] ) ) {
-		if ( $metadata['mime-type'] == 'pdf' ) {
-			$file_collection = array_column( $metadata['sizes'], 'file' );
-		} else {
-			return true;
-		}
+	// if the attachment doesn't contain resizes it isn't an image or document with previews (like pdf)
+	if (empty($metadata['sizes'])) return $metadata;
+
+	// get the image mime
+	$mime = get_post_mime_type($attachment_id);
+
+
+	// if not available add the full image to the file collection (is available only if the attachment is an image)
+	if ( ! empty( $metadata['file'] ) ) {
+		$image_path = $metadata['file'];
 	} else {
-		$file_collection = array_merge( array( $fname . "." . $fext ), array_column( $metadata['sizes'], 'file' ) );
+		$image_path = get_post_meta( $attachment_id, '_wp_attached_file', true );
 	}
 
-	switch ( $fext ) {
-		case 'jpg':
+	list( $basedir, $path, $filename ) = easylazy_explode_filepath(  $image_path );
 
-			$compressionQuality = 82;
+	// initialize the file sub-sizes collection
+	$file_collection = $metadata['sizes'];
 
-			if (extension_loaded('imagick') && class_exists('Imagick')) {
-				$img = new Imagick(  );
-				// the quality of a jpg can be compared with the webp compression with a ratio of 102.5 (82/80)
-				// source:  https://www.industrialempathy.com/posts/avif-webp-quality-settings/#quality-settings-for-a-range-of-jpeg-qualities
-				// getImageCompressionQuality need the exif data to work as intended otherwise will return 82
-				$img->readImage( $basedir . '/' . $path . '/' . $filename );
-				$compressionQuality =  round($img->getImageCompressionQuality() * 1.025);
-			}
+	// the full size image (that is already set for attachment like pdf)
+	if (!isset($file_collection['full'])) $file_collection['full'] = array(
+		'file'      => $filename,
+		'width'     => $metadata['width'],
+		'height'    => $metadata['height'],
+		'mime-type' => $mime
+	);
 
-			foreach ( $file_collection as $value ) {
-				$image = imagecreatefromjpeg( $basedir . '/' . $path . '/' . $value );
+	// determine/set the quality to be used in the case of a jpg image
+	$compressionQuality = EASYLAZY_DEFAULT_JPG_COMPRESSION;
+	if ($mime == 'image/jpeg') {
+		if ( extension_loaded('imagick') && class_exists('Imagick') ) {
+			// the quality of a jpg can be compared with the webp compression with a ratio of 102.5 (82/80)
+			// source:  https://www.industrialempathy.com/posts/avif-webp-quality-settings/#quality-settings-for-a-range-of-jpeg-qualities
+			// getImageCompressionQuality need the exif data to work as intended otherwise will return 82
+			$img = new Imagick($basedir . '/' . $path . '/' . $filename);
+			$compressionQuality =  round($img->getImageCompressionQuality() * 1.025);
+		}
+	}
 
-				imagewebp( $image, $basedir . '/' . $path . '/' . $value . '.webp', $compressionQuality );
+	foreach ( $file_collection as $resize => $file ) {
 
-				imagedestroy( $image );
-			}
+		if ( ! file_exists( $basedir . '/' . $path . '/' . $file['file'] ) ) {
+			error_log( 'EasyLazy: image resize was missing: ' . $file['file'] );
 			break;
+		}
 
-		case 'png':
-			foreach ( $file_collection as $value ) {
+		switch ( $file['mime-type'] ) {
 
-				$image = imagecreatefrompng( $basedir . '/' . $path . '/' . $value );
+			case 'image/jpeg':
+
+				$image = imagecreatefromjpeg( $basedir . '/' . $path . '/' . $file['file'] );
+
+				$compressionQuality = easylazy_is_large_image( $file['width'], $file['height'] ) ? $compressionQuality * EASYLAZY_LARGE_IMAGE_QUALITY_PERCENTUAL : $compressionQuality;
+
+				break;
+
+			case 'image/png':
+
+				$image = imagecreatefrompng( $basedir . '/' . $path . '/' . $file['file'] );
 				imagepalettetotruecolor( $image );
 				imagealphablending( $image, true );
 				imagesavealpha( $image, true );
 
-				imagewebp( $image, $basedir . '/' . $path . '/' . $value . '.webp', 82 );
+				break;
 
-				imagedestroy( $image );
-			}
-			break;
+			case 'image/gif':
 
-		case 'gif':
-			foreach ( $file_collection as $value ) {
-
-				$image = imagecreatefromgif( $basedir . '/' . $path . '/' . $value );
+				$image = imagecreatefromgif( $basedir . '/' . $path . '/' . $file['file'] );
 				imagepalettetotruecolor( $image );
 				imagealphablending( $image, true );
 				imagesavealpha( $image, true );
 
-				imagewebp( $image, $basedir . '/' . $path . '/' . $value . '.webp', 82 );
+				break;
 
-				imagedestroy( $image );
-			}
-			break;
+			default:
 
-		default:
-			return false;
+				return false;
+		}
+
+		// if the webp resize has been created, store it into wordpress metadata array
+		if ( imagewebp( $image, $basedir . '/' . $path . '/' . $file['file'] . '.webp', $compressionQuality ) ) {
+			$metadata['sizes'][ $resize . '_webp' ] = array(
+				'file'      => $file['file'] . '.webp',
+				'width'     => $file['width'],
+				'height'    => $file['height'],
+				'mime-type' => 'image/webp'
+			);
+		}
+
+		// the destroy the image to free some server ram
+		imagedestroy( $image );
+
 	}
 
 	return $metadata;
 }
-add_filter( 'wp_generate_attachment_metadata', 'easylazy_save_webp_copy', 30, 2 );
+add_filter( 'wp_generate_attachment_metadata', 'easylazy_save_webp_copy', 99, 2 );
 
-function easylazy_delete_webp_copy( $post_id ) {
 
-	// get the file path for the image being deleted
-	$metadata = wp_get_attachment_metadata( $post_id );
-
-	list( $basedir, $path, $filename ) = easylazy_explode_filepath( $metadata['file'] );
-	list( $fname, $fext ) = easylazy_explode_filename( $filename );
-
-	// create a fake metadata/size to add the main image to remove list
-	$metadata["sizes"]["full"]["file"] = $fname . "." . $fext;
-
-	// remove the webp copy from using the metadata sizes as iterator
-	foreach ( $metadata['sizes'] as $file ) {
-		if ( isset( $file['file'] ) && file_exists( $basedir . '/' . $path . '/' . $file['file'] . '.webp' ) ) {
-			wp_delete_file( $basedir . '/' . $path . '/' . $file['file'] . '.webp' );
-		}
-	}
-
-	return $post_id;
-}
-add_filter( 'delete_attachment', 'easylazy_delete_webp_copy' );
 
 
 
